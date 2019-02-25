39a736274a8369377bd66591a109202e04f95d22
Megan Cochrane <meg.cochrane9@gmail.com>
2019-02-25 15:47:54 +0000

    Horizontal Scroll - Food tab.

---
+++ DEMO/ImageCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ImageCollectionViewCell.swift
+//  DEMO
+//
+//  Created by Meg Cochrane on 25/02/2019.
+//  Copyright © 2019 Meg Cochrane. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
