b67c551ac2e626fd5001f755e864c3b583df4c09
Megan Cochrane <meg.cochrane9@gmail.com>
2019-03-21 18:37:10 +0000

    WKWebView constraints added.

--- DEMO/HomeViewController.swift
+++ DEMO/HomeViewController.swift
@@ -8,19 +8,24 @@ class HomeViewController: UIViewController, WKUIDelegate {
     
     var webView: WKWebView?
     
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView?.uiDelegate = self
-        view = webView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let myURL = URL(string: "https://megafit.home.blog/")
+        let myURL = URL(string: "https://megafit.home.blog")
         let myRequest = URLRequest(url: myURL!)
-        webView?.load(myRequest)
-}
-
+        setupWKWebViewConstraints()
+        webViewController.load(myRequest)
+    }
+    
+    func setupWKWebViewConstraints() {
+        
+        let paddingConstant:CGFloat = 0
+        
+        webViewController.translatesAutoresizingMaskIntoConstraints = true
+        
+        webViewController.topAnchor.constraint(equalTo: self.view.topAnchor, constant: paddingConstant).isActive = true
+        webViewController.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -paddingConstant).isActive = true
+        webViewController.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: paddingConstant).isActive = true
+        webViewController.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -paddingConstant).isActive = true
+    }
+    
 }
